f48abef7bc8f90a55141311bd98367f97d9bab54
bidduthosen <mdbidduthosen34@gmail.com>
2025-08-05 21:08:26 +0600

    slider add

--- src/Components/Customer/Science/Science.jsx
+++ src/Components/Customer/Science/Science.jsx
@@ -1,11 +1,62 @@
-import React from 'react';
 import scienceImg from '../../../assets/science/science.png'
+import slep from '../../../assets/science/sleep.jpg'
+import slid1 from '../../../assets/science/Rectangle 19.png'
+import slide23 from '../../../assets/science/slide23.jpg'
+import slid5 from '../../../assets/science/slid2.jpg'
 import { HiBadgeCheck } from "react-icons/hi";
+
+import React, { useRef, useState } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+import './Science.css'
+
+
+import { EffectCoverflow, Pagination } from 'swiper/modules';
 
 const Science = () => {
     return (
         <div className='bg-[#6B2B00] py-16'>
             <div className='lg:container mx-auto px-5 md:px-10'>
+
+
+                {/*------------------------ carosel section------------------ */}
+                <Swiper
+                    effect={'coverflow'}
+                    grabCursor={true}
+                    centeredSlides={true}
+                    slidesPerView={'auto'}
+                    coverflowEffect={{
+                        rotate: 50,
+                        stretch: 0,
+                        depth: 100,
+                        modifier: 1,
+                        slideShadows: true,
+                    }}
+                    modules={[EffectCoverflow, Pagination]}
+                    className="mySwiper"
+                >
+                    <SwiperSlide>
+                        <img src={slep} />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img src={slide23} />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img src={slid1} />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img src={slid5} />
+                    </SwiperSlide>
+                </Swiper>
+
+
+
+
+
+                {/* ------------------science section ------------------------- */}
                 <div className='grid grid-cols-1 lg:grid-cols-2 gap-8'>
                     <div className='text-white flex items-center'>
                         <div>
