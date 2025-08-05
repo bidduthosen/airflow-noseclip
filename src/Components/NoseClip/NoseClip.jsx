baa1090e7cb467588467d7c58f514c21a30705b0
bidduthosen <mdbidduthosen34@gmail.com>
2025-08-05 10:42:19 +0600

    nose clip section desing and responsite done and used swiper js npm used

--- src/Components/NoseClip/NoseClip.jsx
+++ src/Components/NoseClip/NoseClip.jsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import clipImg from '../../assets/noseClip.png'
+import React, { useRef, useState } from 'react';
 import { FaArrowRight } from "react-icons/fa6";
 import { FaStar } from "react-icons/fa6";
 import { RiVerifiedBadgeFill } from "react-icons/ri";
@@ -9,14 +8,96 @@ import r from '../../assets/svg/Item → Img (2).svg'
 import shop from '../../assets/svg/Item → Img (3).svg'
 import visa from '../../assets/svg/Item → Img (4).svg'
 
+import nose1 from '../../assets/noseclip/nose1.png'
+import nose2 from '../../assets/noseclip/nose2.png'
+import nose3 from '../../assets/noseclip/nose3.png'
+import nose4 from '../../assets/noseclip/nose4.png'
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/navigation';
+import 'swiper/css/thumbs';
+import { FreeMode, Navigation, Thumbs } from 'swiper/modules';
+import './Noseclip.css'
+
 
 const NoseClip = () => {
+    const [thumbsSwiper, setThumbsSwiper] = useState(null);
     return (
         <div className='lg:container mx-auto px-5 md:px-10 mt-11 mb-12'>
-            <div className='grid grid-cols-1 md:grid-cols-2 gap-10'>
-                <div className=''>
-                    <img src={clipImg} alt="" />
+            <div className='grid grid-cols-1 lg:grid-cols-2 gap-16'>
+
+                {/* slider */}
+                <div>
+                    <Swiper
+                        style={{
+                            '--swiper-navigation-color': '#fff',
+                            '--swiper-pagination-color': '#fff',
+                        }}
+                        spaceBetween={10}
+                        navigation={false}
+                        thumbs={{ swiper: thumbsSwiper }}
+                        modules={[FreeMode, Navigation, Thumbs]}
+                        className="mySwiper2 rounded-xl"
+                    >
+                        <SwiperSlide className='bg-[#f97316]'>
+                            <img src={nose1} />
+                        </SwiperSlide>
+                        <SwiperSlide className='bg-purple-300'>
+                            <img src={nose2} />
+                        </SwiperSlide>
+                        <SwiperSlide className='bg-pink-300'>
+                            <img src={nose3} />
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <img src={nose4} />
+                        </SwiperSlide>
+                        
+                    </Swiper>
+                    <Swiper
+                        onSwiper={setThumbsSwiper}
+                        spaceBetween={10}
+                        slidesPerView={4}
+                        freeMode={true}
+                        watchSlidesProgress={true}
+                        modules={[FreeMode, Navigation, Thumbs]}
+                        className="mySwiper"
+                    >
+                        <SwiperSlide className='bg-[#6B2B00] rounded-xl'>
+                            <img  src={nose1} />
+                        </SwiperSlide>
+                        <SwiperSlide  className=' rounded-xl'>
+                            <img src={nose2} />
+                        </SwiperSlide>
+                        <SwiperSlide  className='bg-[#6B2B00] rounded-xl'>
+                            <img src={nose3} />
+                        </SwiperSlide>
+                        <SwiperSlide  className=' rounded-xl'>
+                            <img src={nose4} />
+                        </SwiperSlide>
+                       
+                      
+                    </Swiper>
                 </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+                {/* <div className=''>
+                    <img src={clipImg} alt="" />
+                </div> */}
+
                 <div>
                     <div className='flex items-center space-x-2 '>
                         <FaStar size={23} className='text-[#FFC107]' /> <FaStar size={23} className='text-[#FFC107]' /><FaStar size={23} className='text-[#FFC107]' /> <FaStar size={23} className='text-[#FFC107]' /> <FaStar size={23} className='text-[#FFC107]' />  <span className='text-lg font-semibold'>4.5</span> <span className='text-base'>(500+ Reviews)</span>
@@ -48,7 +129,7 @@ const NoseClip = () => {
                             <RiVerifiedBadgeFill size={18} className='text-orange-500' />
                             <span className='text-lg font-medium'>100% Drug Free</span>
                         </div>
-                        
+
 
                     </div>
                     <button className="btn bg-[#f97316] rounded-3xl text-white w-full text-base font-semibold my-6 ">Add To Cart <FaArrowRight className='text-white' /></button>
@@ -91,5 +172,6 @@ const NoseClip = () => {
         </div>
     );
 };
+
 
 export default NoseClip;
